713929e3cef5d4f6ed625c7a4f168898be28431c
yash-k-s-7span <yash.k.s@7span.com>
2024-05-15 17:12:44 +0530

    update invite

---
+++ src/component/pages/groupinvite.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom'
+
+const GroupInvite = () => {
+
+    const { id } = useParams();
+    useEffect(() => {
+        checkInvitation();
+    }, [])
+
+
+    async function checkInvitation() {
+        const response = await axios.post(`${import.meta.env.VITE_API}/invite-group/?token=${id}`, {
+            token: id
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        });
+
+        console.log(response)
+    }
+
+    return (
+        <>
+            <div>GroupInvite</div>
+            <p>{id}</p>
+        </>
+    )
+}
+
+export default GroupInvite
